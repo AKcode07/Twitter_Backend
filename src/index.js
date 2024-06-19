17ade3b64c1690890ed4f44dfbdb3357baf91d64
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-19 20:20:33 +0530

    19th June 2024

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const connect = require('./config/database')
 const app = express();
 
+const Tweet = require('./models/tweet')
+
 app.listen(3000, async() => {
   console.log('Server started on port 3000');
   await connect();
   console.log('Mongo db Connected');
+  // const tweet = await Tweet.create({
+  //   content: '3rd tweet',
+  // });
+  // console.log(tweet);
 });
