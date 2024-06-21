b1063c51505773f73a78a9c2fa18d944429a9202
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-21 22:13:06 +0530

    21th June 2024

--- src/models/hastag.js
+++ src/models/hastag.js
@@ -3,7 +3,7 @@ const hashtagSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        // unique: true
+        unique: true
     }, 
     tweets: [
         {
