5b1f3b02bba580d42354984d307d0182959ad3f9
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-21 22:28:04 +0530

    21th June 2024

--- src/index.js
+++ src/index.js
@@ -2,28 +2,12 @@ const express = require('express');
 const connect = require('./config/database')
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-// const Comment = require('./models/comment');
+
 
 app.listen(3000, async() => {
   console.log('Server started on port 3000');
   await connect();
   console.log('Mongo db Connected');
   const tweetRepo = new TweetRepository();
-
-  // const tweet = await tweetRepo.create({
-  //   content: 'Tweet with Comment schema',
-  // });
-  // console.log(tweet);
-  // const comment = await Comment.create({
-  //   content: 'This is a comment'});
-  // tweet.comments.push(comment);
-  // await tweet.save();
-  // console.log(tweet); 
-
-  // const tweet = await tweetRepo.getWithComments('6672f8a2c6fce5f75200512f');
-  // console.log(tweet);
   
-  // const tweet = await tweetRepo.getAll(2,4);
-  // console.log(tweet);
 });
