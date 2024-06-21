f9ed32ad51bfaa3e3cab38b169f9b521dc8d3844
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-21 22:37:16 +0530

    21th June 2024

--- src/index.js
+++ src/index.js
@@ -8,6 +8,5 @@ app.listen(3000, async() => {
   console.log('Server started on port 3000');
   await connect();
   console.log('Mongo db Connected');
-  const tweetRepo = new TweetRepository();
-  
+
 });
