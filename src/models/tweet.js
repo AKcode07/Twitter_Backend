0baf8a1b5c24a45327f5bdb1aac13a931f67fa82
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-21 22:03:37 +0530

    21th June 2024

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -3,17 +3,23 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
+        max: [250, 'Tweet cannot be more than 250 characters']
     },
-    userEmail: {
-        type: String,
-    },
-    comments: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Comment",
-      }
-    ]
-    
+    // likes: [
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: 'Like'
+    //     }
+    // ],
+    // comments: [
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: 'Comment'
+    //     }
+    // ],
+    // image: {
+    //     type: String
+    // }
 }, {timestamps: true});
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
