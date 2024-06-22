3033be1aae757abd66e73649852778cb5ee26c26
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-22 20:29:08 +0530

    22th June 2024

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import { connect } from "./config/database.js";
+import bodyParser from "body-parser";
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+import apiRoutes from "./routes/index.js";
 
 import TweetService from "./services/tweet-service.js";
+
+app.use("/api", apiRoutes);
+
 app.listen(3000, async () => {
   console.log("Server started on port 3000");
   await connect();
   console.log("Mongo db Connected");
+  
   // let service = new TweetService();
   // const tweet = await service.create(
   //   {
