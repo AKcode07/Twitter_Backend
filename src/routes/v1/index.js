9fef9371f70523d6a5d01e3e45e7ef8de2149501
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-24 21:45:36 +0530

    23rd June 2024

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,7 +4,7 @@ import {
   createTweet,
   // getTweet
 } from "../../controllers/tweet-controller.js";
-// import { toggleLike } from "../../controllers/like-controller.js";
+import { toggleLike } from "../../controllers/like-controller.js";
 // import { createComment } from "../../controllers/comment-controller.js";
 // import { signup, login } from "../../controllers/auth-controller.js";
 
@@ -15,7 +15,7 @@ const router = express.Router();
 router.post("/tweets", createTweet);
 // router.get("/tweets/:id", getTweet);
 
-// router.post("/likes/toggle", toggleLike);
+router.post("/likes/toggle", toggleLike);
 
 // router.post("/comments", authenticate, createComment);
 
