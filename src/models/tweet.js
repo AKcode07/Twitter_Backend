033ce26244dcf95f71a206eb8202415e0ebb11f4
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-24 22:14:24 +0530

    24th June 2024

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -12,12 +12,12 @@ const tweetSchema = new mongoose.Schema(
             ref: 'Like'
         }
     ],
-    // comments: [
-    //     {
-    //         type: mongoose.Schema.Types.ObjectId,
-    //         ref: 'Comment'
-    //     }
-    // ],
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ],
     // image: {
     //     type: String
     // }
