c537d5796dd2728813a34968f0232eb66335548d
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-25 22:45:13 +0530

    25th June 2024

--- src/index.js
+++ src/index.js
@@ -1,36 +1,24 @@
 import express from "express";
-import { connect } from "./config/database.js";
 import bodyParser from "body-parser";
+import passport from "passport";
+
+import { connect } from "./config/database.js";
+
+import { passportAuth } from "./config/jwt-middleware.js";
+
+import apiRoutes from "./routes/index.js";
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-import apiRoutes from "./routes/index.js";
-
-import { UserRepository,TweetRepository } from "./repository/index.js";
-import LikeService from "./services/like-service.js";
+app.use(passport.initialize());
+passportAuth(passport);
 
 app.use("/api", apiRoutes);
 
 app.listen(3000, async () => {
-  console.log("Server started on port 3000");
+  console.log("server started");
   await connect();
-  console.log("Mongo db Connected");
-  
-  // const userRepo = new UserRepository();
-  // const tweetRepo = new TweetRepository();
-  // const tweets =await tweetRepo.getAll(0, 10);
-  // const user = await userRepo.create({
-  //   email: 'Ashwin@admin.com',
-  //   password: '12345',
-  //   name: 'Ashwin'
-  // });
-  
-  // const likeservice = new LikeService();
-  // await likeservice.toggleLike(
-  //   tweets[0].id,
-  //   "Tweet",
-  //   "667843730eb399c6c3bef038"
-  // );
+  console.log("Mongo db connected");
 });
