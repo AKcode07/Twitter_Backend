dda1d076ef4dd692830087d7286c642169b7126a
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-26 16:20:22 +0530

    26th June 2024

--- src/config/file-upload-s3-config.js
+++ src/config/file-upload-s3-config.js
@@ -1,17 +1,19 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import aws from "aws-sdk";
+import { S3Client } from "@aws-sdk/client-s3";
+import { fromEnv } from "@aws-sdk/credential-provider-env";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-aws.config.update({
-  region: process.env.AWS_REGION,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  accessKeyId: process.env.ACCESS_KEY_ID,
-});
+// console.log("AWS_REGION:", process.env.AWS_REGION);
+// console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
+// console.log("AWS_SECRET_ACCESS_KEY:", process.env.AWS_SECRET_ACCESS_KEY);
 
-const s3 = new aws.S3();
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: fromEnv(),
+});
 
 const upload = multer({
   storage: multerS3({
